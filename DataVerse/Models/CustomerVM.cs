cc0aa2432050151a2a02213958d088b5580cd55b
Theodore Stassinos <thestas@yahoo.com>
2022-11-07 20:38:09 +0200

    Added jquery validation to insert Customer

---
+++ DataVerse/Models/CustomerVM.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DataVerse.Models
+{
+    public class CustomerVM
+    {
+      
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        
+        public string Address { get; set; }
+
+       
+        public string email { get; set; }
+        public int id { get; set; }
+
+       
+        public Nullable<long> HomePhone { get; set; }
+
+       
+        public Nullable<long> WorkPhone { get; set; }
+
+       
+        public Nullable<long> CellPhone { get; set; }
+    }
+}
